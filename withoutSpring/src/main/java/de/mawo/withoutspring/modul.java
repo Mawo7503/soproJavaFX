4060285efc2da8e98a838dd8363c51e538937e36
Marius Wolff <marius.wolff03@web.de>
2023-06-27 15:36:18 +0200

    Added

---
+++ withoutSpring/src/main/java/de/mawo/withoutspring/modul.java
@@ -0,0 +1,43 @@
+package de.mawo.withoutspring;
+/**
+ * Nothing special to see here.
+ * Only important thing to know here:
+ * If you want to use a Tableview, remember that Tableview.setItems uses a .get*VarName*() standard, so a Record will create Errors!
+ * */
+public class modul {
+
+    private String modulname;
+    private int lp;
+    private Double note;
+
+    public modul(String name, int lp, double note) {
+        this.modulname = name;
+        this.lp = lp;
+        this.note = note;
+    }
+
+    public String getModulname() {
+        return modulname;
+    }
+
+    public void setModulname(String modulname) {
+        this.modulname = modulname;
+    }
+
+    public int getLp() {
+        return lp;
+    }
+
+    public void setLp(int lp) {
+        this.lp = lp;
+    }
+
+    public Double getNote() {
+        return note;
+    }
+
+    public void setNote(Double note) {
+        this.note = note;
+    }
+
+}
